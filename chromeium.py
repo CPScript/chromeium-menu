8d2261373ef48402efd6577db228c9ad34146afc
Disease <83523587+CPScript@users.noreply.github.com>
2024-01-18 14:46:40 -0500

    Create chromeium.py

---
+++ chromeium.py
@@ -0,0 +1,9 @@
+import os
+from os import system
+import time
+from webbot import Browser
+print("Installation complete")
+time.sleep(1)
+os.system('clear')
+print("Please wait!")
+Chromium = Browser()
